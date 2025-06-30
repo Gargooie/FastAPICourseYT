7452fe4c360d6b9ad198c17669e8efc2a8c9db28
Dunaeff Eugenii <56464554+Gargooie@users.noreply.github.com>
2025-06-30 09:11:53 +0400

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+
+from fastapi import FastAPI
+from blog import schemas
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return "Hello World"
+
+@app.get("/blog")
+def say_hello(limit):
+    return f"{limit} of books"
+
+@app.get("/blog/{id}")
+def say_hello(id: int):
+    return {"message": id}
+
+
+@app.post("/blog")
+def create_blog(blog: schemas.Blog):
+    return {'data': f"blog created with {blog.title}"}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
